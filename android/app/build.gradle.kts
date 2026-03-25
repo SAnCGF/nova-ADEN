e32127d9431e4ce8c1a6e0f92b2c0d9423902189
SAnCGF <sanchezfreddy0382428@gmail.com>
2026-03-25 04:39:37 +0000

    fix: Android Embedding V2 - Forzar actualización de archivos para GitHub Actions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,40 +1,8 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
+plugins { id("com.android.application"); id("kotlin-android"); id("dev.flutter.flutter-gradle-plugin") }
+android { namespace = "com.example.nova_aden"; compileSdk = flutter.compileSdkVersion
+  compileOptions { sourceCompatibility = JavaVersion.VERSION_1_8; targetCompatibility = JavaVersion.VERSION_1_8 }
+  kotlinOptions { jvmTarget = JavaVersion.VERSION_1_8.toString() }
+  defaultConfig { applicationId = "com.example.nova_aden"; minSdk = flutter.minSdkVersion; targetSdk = flutter.targetSdkVersion; versionCode = flutter.versionCode; versionName = flutter.versionName }
+  buildTypes { release { isMinifyEnabled = false; signingConfig = signingConfigs.getByName("debug") } }
 }
-
-android {
-    namespace = "com.example.nova_aden"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
-
-    defaultConfig {
-        applicationId = "com.example.nova_aden"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            isShrinkResources = false
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
-}
+flutter { source = "../.." }
--- android/app/src/main/kotlin/com/example/nova_aden/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nova_aden/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.example.nova_aden
-
 import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity()
